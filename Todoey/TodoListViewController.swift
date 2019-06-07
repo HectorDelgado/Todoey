8c84322a1cddf1cde7ded7328a9f93b963f48aae
Hector Delgado <Hector.d.delgado@gmail.com>
2019-06-06 17:12:39 -0700

    Data persisted using UserDefaults method.

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -11,30 +11,34 @@ import UIKit
 class TodoListViewController: UITableViewController {
     
     var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    
+    let userDefaultsName = "TodoListArray"
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.delegate = self
-        tableView.dataSource = self
+        if let items = defaults.array(forKey: userDefaultsName) as? [String] {
+            itemArray = items
+        }
     }
     
     //Mark -- Define various propeties for the TableView
     
-    
+    // Determines number of rows to display
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
     
+    // Returns a UITableViewCell inflated with data from itemArray
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
-        
         cell.textLabel?.text = itemArray[indexPath.row]
-        
         return cell
     }
     
+    // Toggle between checked and unchecked accessory for list item
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
@@ -42,25 +46,27 @@ class TodoListViewController: UITableViewController {
         } else {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
+        
         tableView.deselectRow(at: indexPath, animated: true)
     }
 
     //Mark - Add new list item
     
-    
+    // Uses UIAlertController to attempt to add a new item to the list.
+    // Adds nothing if the user cancels the action or the textfield is empty
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
         var textField = UITextField()
         
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         
-        let action = UIAlertAction(title: "Add Item", style: .default, handler: { (action) in
-            
+        let addItemAction = UIAlertAction(title: "Add Item", style: .default, handler: { (action) in
             if textField.text?.isEmpty ?? true {
                 print("TextField is empty")
-                
             } else {
                 self.itemArray.append(textField.text!)
+                
+                self.defaults.set(self.itemArray, forKey: self.userDefaultsName)
                 self.tableView.reloadData()
             }
         })
@@ -73,11 +79,10 @@ class TodoListViewController: UITableViewController {
             alertTextField.placeholder = "Create new item"
             textField = alertTextField
         }
-        alert.addAction(action)
+        alert.addAction(addItemAction)
         alert.addAction(cancelAction)
         
         present(alert, animated: true, completion: nil)
     }
-    
 }
 
