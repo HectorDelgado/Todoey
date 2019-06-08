0165aa39b65f17eb1f820259202a0ea007e61679
Hector Delgado <Hector.d.delgado@gmail.com>
2019-06-08 13:05:20 -0700

    Persist data using Codable and Plist.

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -10,39 +10,26 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray = [Item]()
+    var itemArray = [Item]()    // Array of Item objects for todo-list
     
-    let userDefaultsName = "TodoListArray"  // Name of UserDefaults storage
-    let defaults = UserDefaults.standard    // Standard instance of UserDefaults
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        .first?.appendingPathComponent( "Items.plist")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let newItem1 = Item()
-        let newItem2 = Item()
-        let newItem3 = Item()
-        
-        newItem1.title = "Find Mike"
-        newItem2.title = "Buy Eggos"
-        newItem3.title = "Destory Demogorgon"
-        
-        itemArray.append(newItem1)
-        itemArray.append(newItem2)
-        itemArray.append(newItem3)
-        
-        if let storedItems = defaults.array(forKey: userDefaultsName) as? [Item] {
-            itemArray = storedItems
-        }
+        loadItems()
     }
     
-    //Mark -- Define various propeties for the TableView
     
-    // Determines number of rows to display
+    //MARK: - Define various propeties for the TableView
+    
+    // Determines number of rows to display in TableView
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
     
-    // Returns a UITableViewCell inflated with data from itemArray
+    // Inflates a reusable TableView cell with a list item
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
@@ -52,16 +39,15 @@ class TodoListViewController: UITableViewController {
         return cell
     }
     
-    // Toggle between checked and unchecked accessory for list item
+    // Toggle checkmark when a row is clicked
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         itemArray[indexPath.row].isDone = !itemArray[indexPath.row].isDone
-        
-        tableView.reloadData()
+        saveItems()
         tableView.deselectRow(at: indexPath, animated: true)
     }
 
-    //Mark - Add new list item
+    //MARK: - Add new list item
     
     // Uses UIAlertController to attempt to add a new item to the list.
     // Adds nothing if the user cancels the action or the textfield is empty
@@ -74,15 +60,13 @@ class TodoListViewController: UITableViewController {
         let addItemAction = UIAlertAction(title: "Add Item", style: .default, handler: { (action) in
             if textField.text?.isEmpty ?? true {
                 print("TextField is empty")
-                
-                
             } else {
                 let newItem = Item()
                 newItem.title = textField.text!
                 
                 self.itemArray.append(newItem)
-                self.defaults.set(self.itemArray, forKey: self.userDefaultsName)
-                self.tableView.reloadData()
+                
+                self.saveItems()
             }
         })
         
@@ -98,6 +82,31 @@ class TodoListViewController: UITableViewController {
         alert.addAction(cancelAction)
         
         present(alert, animated: true, completion: nil)
+    }
+    
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let newData = try encoder.encode(itemArray)
+            try newData.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding item array. \(error)")
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("Error decoding item array. \(error)")
+            }
+        }
     }
 }
 
